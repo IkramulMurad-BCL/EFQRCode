79389b3cf56494851a2582df08867a57f1f6eee7
Md. Ikramul Murad <murad@braincraftapps.com>
2025-10-13 15:34:29 +0600

    Update visual fill class

--- Source/Styles/CustomSVG/VisualFill.swift
+++ Source/Styles/CustomSVG/VisualFill.swift
@@ -12,13 +12,19 @@ public protocol VisualFill {
 }
 
 public class SolidColor: VisualFill {
-    
+    public init(with hex: String) {
+        
+    }
 }
 
 public class LinearGradient: VisualFill {
-    
+    public init(with hex1: String, hex2: String) {
+        
+    }
 }
 
 public class ImageMask: VisualFill {
-    
+    public init(with image: UIImage) {
+        
+    }
 }
