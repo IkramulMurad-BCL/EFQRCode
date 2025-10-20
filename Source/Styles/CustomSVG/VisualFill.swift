2119ce3a7282f84c3b16549ed1b28f43abd1613a
Md. Ikramul Murad <murad@braincraftapps.com>
2025-10-20 09:09:34 +0600

    Update code

--- Source/Styles/CustomSVG/VisualFill.swift
+++ Source/Styles/CustomSVG/VisualFill.swift
@@ -12,19 +12,27 @@ public protocol VisualFill {
 }
 
 public class SolidColor: VisualFill {
+    public let color: UIColor
+
     public init(with hex: String) {
-        
+        self.color = UIColor(hex: hex)
     }
 }
 
 public class LinearGradient: VisualFill {
+    public let startColor: UIColor
+    public let endColor: UIColor
+
     public init(with hex1: String, hex2: String) {
-        
+        self.startColor = UIColor(hex: hex1)
+        self.endColor = UIColor(hex: hex2)
     }
 }
 
 public class ImageMask: VisualFill {
+    public let image: UIImage
+
     public init(with image: UIImage) {
-        
+        self.image = image
     }
 }
