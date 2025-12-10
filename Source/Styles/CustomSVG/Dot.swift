1d837d166af9959011db7fc44251182386f32bcb
Md. Ikramul Murad <murad@braincraftapps.com>
2025-12-10 15:20:18 +0600

    Update dot

--- Source/Styles/CustomSVG/Dot.swift
+++ Source/Styles/CustomSVG/Dot.swift
@@ -169,7 +169,7 @@ public struct AssetLess: Dot {
         var nx = x + 1
 
         while nx < nCount,
-              qrCode.model.isDark(x, y),
+              qrCode.model.isDark(nx, y),
               available[nx][y] {
             length += 1
             nx += 1
@@ -177,13 +177,34 @@ public struct AssetLess: Dot {
 
         // mark consumed
         for dx in 0..<length { available[x+dx][y] = false }
-
-        // Draw line from (x, y) to (x + length, y)
-        let cap = svgLineCap()
-        let str = """
-            <line key="\(idCount)" x1="\(x.cgFloat)" y1="\(y.cgFloat+0.5)" x2="\(x.cgFloat+length.cgFloat)" y2="\(y.cgFloat+0.5)" stroke="black" stroke-width="1" stroke-linecap="\(cap)"/>
-        """
-        pointList.append(str)
+        
+        var svg = ""
+        if length == 1 {
+            svg.append(getSingleUnit(x: x, y: y, lineCap: lineCap, idCount: idCount))
+        } else {
+            let startX = CGFloat(x)
+            let endX = CGFloat(x + length - 1)
+            let yPos = CGFloat(y)
+            
+            svg += getHorizontalStartCap(x: startX, y: yPos, lineCap: lineCap, id: idCount)
+            idCount += 1
+            
+            if length > 2 {
+                let midX = startX + 1
+                let midWidth = CGFloat(length - 2)
+                
+                svg += """
+                    <rect key="\(idCount)" x="\(midX)" y="\(yPos)" width="\(midWidth)" height="1"/>
+                    """
+                idCount += 1
+            }
+            
+            
+            svg += getHorizontalEndCap(x: endX, y: yPos, lineCap: lineCap, id: idCount)
+            idCount += 1
+        }
+        
+        pointList.append(svg)
         idCount += 1
     }
 
@@ -196,7 +217,7 @@ public struct AssetLess: Dot {
         var ny = y + 1
 
         while ny < nCount,
-              qrCode.model.isDark(x, y),
+              qrCode.model.isDark(x, ny),
               available[x][ny] {
             length += 1
             ny += 1
@@ -204,11 +225,33 @@ public struct AssetLess: Dot {
 
         for dy in 0..<length { available[x][y+dy] = false }
 
-        let cap = svgLineCap()
-        let str = """
-            <line key="\(idCount)" x1="\(x.cgFloat+0.5)" y1="\(y.cgFloat)" x2="\(x.cgFloat+0.5)" y2="\(y.cgFloat+length.cgFloat)" stroke="black" stroke-width="1" stroke-linecap="\(cap)"/>
-        """
-        pointList.append(str)
+        var svg = ""
+        if length == 1 {
+            svg.append(getSingleUnit(x: x, y: y, lineCap: lineCap, idCount: idCount))
+        } else {
+            let xPos = CGFloat(x)
+            let endY = CGFloat(y + length - 1)
+            let startY = CGFloat(y)
+            
+            svg += getVerticalStartCap(x: xPos, y: startY, lineCap: lineCap, id: idCount)
+            idCount += 1
+            
+            if length > 2 {
+                let midY = startY + 1
+                let midHeight = CGFloat(length - 2)
+                
+                svg += """
+                    <rect key="\(idCount)" x="\(xPos)" y="\(midY)" width="1" height="\(midHeight)"/>
+                    """
+                idCount += 1
+            }
+            
+            
+            svg += getVerticalEndCap(x: xPos, y: endY, lineCap: lineCap, id: idCount)
+            idCount += 1
+        }
+        
+        pointList.append(svg)
         idCount += 1
     }
 
@@ -222,7 +265,7 @@ public struct AssetLess: Dot {
         var ny = y + 1
 
         while nx < nCount, ny < nCount,
-              qrCode.model.isDark(x, y),
+              qrCode.model.isDark(nx, ny),
               available[nx][ny] {
             length += 1
             nx += 1; ny += 1
@@ -230,11 +273,11 @@ public struct AssetLess: Dot {
 
         for i in 0..<length { available[x+i][y+i] = false }
 
-        let cap = svgLineCap()
-        let str = """
-            <line key="\(idCount)" x1="\(x.cgFloat+0.3)" y1="\(y.cgFloat+0.3)" x2="\(x.cgFloat+length.cgFloat-0.3)" y2="\(y.cgFloat+length.cgFloat-0.3)" stroke="black" stroke-width="1" stroke-linecap="\(cap)"/>
-        """
-        pointList.append(str)
+//        let cap = svgLineCap()
+//        let str = """
+//            <line key="\(idCount)" x1="\(x.cgFloat+0.3)" y1="\(y.cgFloat+0.3)" x2="\(x.cgFloat+length.cgFloat-0.3)" y2="\(y.cgFloat+length.cgFloat-0.3)" stroke="black" stroke-width="1" stroke-linecap="\(cap)"/>
+//        """
+//        pointList.append(str)
         idCount += 1
     }
 
@@ -248,7 +291,7 @@ public struct AssetLess: Dot {
         var ny = y + 1
 
         while nx >= 0, ny < nCount,
-              qrCode.model.isDark(x, y),
+              qrCode.model.isDark(nx, ny),
               available[nx][ny] {
             length += 1
             nx -= 1; ny += 1
@@ -256,19 +299,115 @@ public struct AssetLess: Dot {
 
         for i in 0..<length { available[x-i][y+i] = false }
 
-        let cap = svgLineCap()
-        let str = """
-            <line key="\(idCount)" x1="\(x.cgFloat+0.7)" y1="\(y.cgFloat+0.3)" x2="\(x.cgFloat-length.cgFloat+0.7)" y2="\(y.cgFloat+length.cgFloat-0.3)" stroke="black" stroke-width="1" stroke-linecap="\(cap)"/>
-        """
-        pointList.append(str)
+//        let cap = svgLineCap()
+//        let str = """
+//            <line key="\(idCount)" x1="\(x.cgFloat+0.7)" y1="\(y.cgFloat+0.3)" x2="\(x.cgFloat-length.cgFloat+0.7)" y2="\(y.cgFloat+length.cgFloat-0.3)" stroke="black" stroke-width="1" stroke-linecap="\(cap)"/>
+//        """
+//        pointList.append(str)
         idCount += 1
     }
 
-    private func svgLineCap() -> String {
+    func getSingleUnit(x: Int, y: Int, lineCap: AssetLessDotLineCap, idCount: Int) -> String {
         switch lineCap {
-        case .none: return "butt"
-        case .angular: return "square"
-        case .rounded: return "round"
+        case .rounded:
+            return "<circle key=\"\(idCount)\" cx=\"\(x.cgFloat + 0.5)\" cy=\"\(y.cgFloat + 0.5)\" r=\"0.5\"/>"
+        case .angular:
+            let p1 = "\(x.cgFloat + 0.5),\(y.cgFloat)"         // Top
+            let p2 = "\(x.cgFloat + 1),\(y.cgFloat + 0.5)"     // Right
+            let p3 = "\(x.cgFloat + 0.5),\(y.cgFloat + 1)"     // Bottom
+            let p4 = "\(x.cgFloat),\(y.cgFloat + 0.5)"         // Left
+            
+            return """
+                    <polygon key="\(idCount)"
+                             points="\(p1) \(p2) \(p3) \(p4)"
+                             />
+                    """
+        default:
+            return "<rect key=\"\(idCount)\" x=\"\(x)\" y=\"\(y)\" width=\"1\" height=\"1\"/>"
         }
     }
+    
+    func getHorizontalStartCap(x: CGFloat, y: CGFloat, lineCap: AssetLessDotLineCap, id: Int) -> String {
+        switch lineCap {
+        case .angular:
+            return """
+            <polygon key="\(id)" points="\(x),\(y+0.5) \(x+0.5),\(y) \(x+1),\(y) \(x+1),\(y+1) \(x+0.5),\(y+1)"/>
+            """
+        case .rounded:
+            return """
+                <path key="\(id)" d="M \(x+0.5) \(y)
+                                     A 0.5 0.5 0 0 0 \(x+0.5) \(y+1)
+                                     Z" />
+
+                <rect key="\(id)_r" x="\(x+0.5)" y="\(y)" width="0.5" height="1"/>
+                """
+        case .none:
+            return """
+            <rect key="\(id)" x="\(x)" y="\(y)" width="1" height="1"/>
+            """
+        }
+    }
+    
+    func getHorizontalEndCap(x: CGFloat, y: CGFloat, lineCap: AssetLessDotLineCap, id: Int) -> String {
+        switch lineCap {
+        case .angular:
+            return """
+            <polygon key="\(id)" points="\(x),\(y) \(x+0.5),\(y) \(x+1),\(y+0.5) \(x+0.5),\(y+1) \(x),\(y+1)"/>
+            """
+        case .rounded:
+            return """
+            <rect key="\(id)_l" x="\(x)" y="\(y)" width="0.5" height="1"/>
+            <path key="\(id)" d="M \(x+0.5) \(y) A 0.5 0.5 0 0 1 \(x+0.5) \(y+1) Z" />
+            """
+        case .none:
+            return """
+            <rect key="\(id)" x="\(x)" y="\(y)" width="1" height="1"/>
+            """
+        }
+    }
+    
+    func getVerticalStartCap(x: CGFloat, y: CGFloat, lineCap: AssetLessDotLineCap, id: Int) -> String {
+        switch lineCap {
+        case .angular:
+            // Diamond, top of vertical line
+            return """
+            <polygon key="\(id)" points="\(x+0.5),\(y) \(x+1),\(y+0.5) \(x+1),\(y+1) \(x),\(y+1) \(x),\(y+0.5)"/>
+            """
+        case .rounded:
+            // Top half-circle, facing up
+            return """
+            <path key="\(id)" d="M \(x) \(y+0.5)
+                                 A 0.5 0.5 0 0 1 \(x+1) \(y+0.5)
+                                 Z" />
+            <rect key="\(id)_b" x="\(x)" y="\(y+0.5)" width="1" height="0.5"/>
+            """
+        case .none:
+            return """
+            <rect key="\(id)" x="\(x)" y="\(y)" width="1" height="1"/>
+            """
+        }
+    }
+
+    func getVerticalEndCap(x: CGFloat, y: CGFloat, lineCap: AssetLessDotLineCap, id: Int) -> String {
+        switch lineCap {
+        case .angular:
+            // Diamond, bottom of vertical line
+            return """
+            <polygon key="\(id)" points="\(x),\(y) \(x+1),\(y) \(x+1),\(y+0.5) \(x+0.5),\(y+1) \(x),\(y+0.5)"/>
+            """
+        case .rounded:
+            // Bottom half-circle, facing down
+            return """
+            <rect key="\(id)_t" x="\(x)" y="\(y)" width="1" height="0.5"/>
+            <path key="\(id)" d="M \(x) \(y+0.5)
+                                 A 0.5 0.5 0 0 0 \(x+1) \(y+0.5)
+                                 Z" />
+            """
+        case .none:
+            return """
+            <rect key="\(id)" x="\(x)" y="\(y)" width="1" height="1"/>
+            """
+        }
+    }
+
 }
